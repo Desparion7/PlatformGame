6c667861207f98a74156d8df40ef3db79d3b8157
wosmateusz3@gmail.com <wosmateusz3@gmail.com>
2023-07-29 17:13:12 +0200

    create basic game

---
+++ main.js
@@ -0,0 +1,77 @@
+import Player from './player.js';
+import { InputHandler } from './input.js';
+import { Background } from './background.js';
+import { FlyingEnemy, ClimbingEnemy, GroundEnemy } from './enemy.js';
+import { UI } from './ui.js';
+
+window.addEventListener('DOMContentLoaded', function () {
+	const canvas = document.getElementById('canvas1');
+	const ctx = canvas.getContext('2d');
+	canvas.width = 500;
+	canvas.height = 500;
+
+	class Game {
+		constructor(width, height) {
+			this.width = width;
+			this.height = height;
+			this.groundMargin = 80;
+			this.speed = 0;
+			this.maxSpeed = 6;
+			this.backgorund = new Background(this);
+			this.player = new Player(this);
+			this.input = new InputHandler(this);
+			this.Ui = new UI(this);
+			this.enemies = [];
+			this.enemyTimer = 0;
+			this.enemyInterval = 1000;
+			this.debug = true;
+			this.score = 0;
+			this.fontColor = 'black';
+		}
+		update(deltaTime) {
+			this.backgorund.update();
+			this.player.update(this.input.keys, deltaTime);
+			// adding enemies
+			if (this.enemyTimer > this.enemyInterval) {
+				this.addEnemy();
+				this.enemyTimer = 0;
+			} else this.enemyTimer += deltaTime;
+			this.enemies.forEach((enemy) => {
+				enemy.update(deltaTime);
+				this.enemies = this.enemies.filter(
+					(enemy) => !enemy.markedForDeletion
+				);
+			});
+		}
+		draw(context) {
+			this.backgorund.draw(context);
+			this.player.draw(context);
+			this.enemies.forEach((enemy) => {
+				enemy.draw(context);
+			});
+			this.Ui.draw(context);
+		}
+		addEnemy() {
+			if (this.speed > 0 && Math.random() < 0.5) {
+				this.enemies.push(new GroundEnemy(this));
+			} else if (this.speed > 0) {
+				this.enemies.push(new ClimbingEnemy(this));
+			}
+			this.enemies.push(new FlyingEnemy(this));
+		}
+	}
+
+	const game = new Game(canvas.width, canvas.height);
+
+	let lastTime = 0;
+	function animate(timeStamp) {
+		const deltaTime = timeStamp - lastTime;
+		lastTime = timeStamp;
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		game.update(deltaTime);
+		game.draw(ctx);
+		requestAnimationFrame(animate);
+	}
+
+	animate(0);
+});
